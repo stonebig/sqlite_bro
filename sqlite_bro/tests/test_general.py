892542379166e1447a579bb47e0c64b91d13a4ef
stonebig <stonebig>
2015-04-15 19:27:21 +0200

    a real (test) bug detected per AppVeyor

--- sqlite_bro/tests/test_general.py
+++ sqlite_bro/tests/test_general.py
@@ -17,9 +17,9 @@ def test_Basics():
 create table item (ItemNo, Description,Kg  , PRIMARY KEY (ItemNo));
 INSERT INTO item values("T","Ford",1000);
 INSERT INTO item select "A","Merced",1250 union all select "W","Wheel",9 ;
-.once 'this_file_of_result.tmp'
+.once '%s'
 select ItemNo, Description, 1000*Kg Gramm  from item order by ItemNo desc;
-.import '%s' in_this_table""" % tmp_file
+.import '%s' in_this_table""" % (tmp_file, tmp_file)
     app.n.new_query_tab("Welcome", welcome_text)
     app.run_tab()
     app.close_db
